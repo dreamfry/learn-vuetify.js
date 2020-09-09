84fde99ee92117db44ccc64c0a6011dc313930b2
dreamfry <784342359@qq.com>
2020-09-09 17:35:02 +0800

    test

--- admin/src/views/Home.vue
+++ admin/src/views/Home.vue
@@ -1,10 +1,13 @@
 <template>
   <v-app id="home">
-    <v-app-bar app>app-bar</v-app-bar>
+    <v-app-bar app class="ms-4 text-overline font-italic" dir="rtl">app-bar</v-app-bar>
+    <v-navigation-drawer class="d-xs-none d-sm-flex" app>
+      1
+    </v-navigation-drawer>
     <v-main>
-      <v-container>
-        <v-row>
-          <v-col cols="6" md="4">block1</v-col>
+      <v-container fluid>
+        <v-row class="blue-grey lighten-5">
+          <v-col cols="6" md="4" class="cyan--text">block1</v-col>
           <v-col cols="6" md="4">block2</v-col>
           <v-col cols="6" md="4">block3</v-col>
           <v-col cols="6" md="4">block4</v-col>
