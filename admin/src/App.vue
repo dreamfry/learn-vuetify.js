4eb485251de35ad587bb43719364e85c7522a8fe
dreamfry <784342359@qq.com>
2020-09-09 15:29:58 +0800

    test

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -1,22 +1,16 @@
 <template>
-  <v-app id="app">
-    <v-system-bar>system-bar</v-system-bar>
-    <v-app-bar>app-bar</v-app-bar>
-    <v-navigation-drawer>navigation-drawer</v-navigation-drawer>
-    <v-bottom-navigation>bottom-navigation</v-bottom-navigation>
-    <v-footer>footer</v-footer>
-    <!-- <router-view></router-view> -->
-  </v-app>
+  <div>
+    <router-view></router-view>
+  </div>
 </template>
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator'
+import { Vue, Component } from "vue-property-decorator";
 @Component({})
 export default class App extends Vue {
-  created(){
-    this.$vuetify.theme.dark = true
+  created() {
+    // this.$vuetify.theme.dark = true
   }
 }
 </script>
 <style lang="scss">
-
 </style>
--- admin/src/views/Home.vue
+++ admin/src/views/Home.vue
@@ -1,19 +1,18 @@
 <template>
-  <div class="home">
-    <v-card class="mx-auto" max-width="120px">
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title>vapp</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-      <v-list-item two-line>
-        <v-list-item-content>
-          <v-list-item-title>vcontainer</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-    </v-card>
-    <router-view></router-view>
-  </div>
+  <v-app id="home">
+    <v-app-bar app>app-bar</v-app-bar>
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col cols="6" md="4">block1</v-col>
+          <v-col cols="6" md="4">block2</v-col>
+          <v-col cols="6" md="4">block3</v-col>
+          <v-col cols="6" md="4">block4</v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+    <v-footer app>footer</v-footer>
+  </v-app>
 </template>
 
 <script lang="ts">
