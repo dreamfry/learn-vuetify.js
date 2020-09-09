c8c062cba2936a8ce5a1befa5c962e28a689c8dc
dreamfry <784342359@qq.com>
2020-09-09 11:00:40 +0800

    test

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # learn-vuetify.js
 my learning process for vuetify.js
+
+# 新建项目
+```bash
+vue add <project-name>
+```
+
+# 初始化环境
+```bash
+vue add vuetify
+vue add router
+vue add typescript
+```
+
+# 解决 vscode 报错
+在文件 <span>tsconfig.json</span> 添加 "vuetify"
+```json
+{
+  "types": [
+    "webpack-env",
+    "vuetify",
+  ]
+}
+```
---
+++ admin/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <v-card class="mx-auto" max-width="120px">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>vapp</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item two-line>
+        <v-list-item-content>
+          <v-list-item-title>vcontainer</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-card>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+@Component({})
+export default class Home extends Vue {}
+</script>
