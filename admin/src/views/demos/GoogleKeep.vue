b18fb59ec2890b866cc0b40781b376e4ba20232f
dreamfry <784342359@qq.com>
2020-09-14 14:29:50 +0800

    test

--- admin/src/views/demos/GoogleKeep.vue
+++ admin/src/views/demos/GoogleKeep.vue
@@ -1,39 +1,46 @@
 <template>
-  <div class="baseline-flipped">
-    <v-navigation-drawer v-model="drawer" app absolute>
-      <v-list dense>
-        <v-list-item>
-          <v-list-item-action>
-            <v-icon>mdi-home</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
+  <v-app id="keep">
+    <v-app-bar app clipped-left color="amber">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <span class="title ml-3 mr-5">
+        Google&nbsp;
+        <span class="font-weight-light">Keep</span>
+      </span>
+      <v-text-field solo-inverted flat hide-details label="Search" prepend-inner-icon="search"></v-text-field>
 
-        <v-list-item>
-          <v-list-item-action>
-            <v-icon>mdi-email</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Contact</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app clipped color="grey lighten-4">
+      <v-list dense class="grey lighten-4">
+        <template v-for="(item, i) in items">
+          <v-row v-if="item.heading" :key="i" align="center">
+            <v-col cols="6">
+              <v-subheader v-if="item.heading">{{ item.heading }}</v-subheader>
+            </v-col>
+            <v-col cols="6" class="text-right">
+              <v-btn small text>edit</v-btn>
+            </v-col>
+          </v-row>
+          <v-divider v-else-if="item.divider" :key="i" dark class="my-4"></v-divider>
+          <v-list-item v-else :key="i" link>
+            <v-list-item-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title class="grey--text">{{ item.text }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar app color="cyan" dark absolute>
-      <v-spacer></v-spacer>
-      <v-toolbar-title>Application</v-toolbar-title>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-    </v-app-bar>
-
     <v-main>
-      <v-container class="fill-height" fluid>
-        <v-row align="center" justify="center">
-          <v-col class="text-center">
-            <v-tooltip left>
-              <template v-slot:activation="{on}">
+      <v-container fluid class="grey lighten-4 fill-height">
+        <v-row justify="center" align="center">
+          <v-col class="shrink">
+            <v-tooltip right>
+              <template v-slot:activator="{ on }">
                 <v-btn :href="source" icon large target="_blank" v-on="on">
                   <v-icon large>mdi-code-tags</v-icon>
                 </v-btn>
@@ -44,26 +51,39 @@
         </v-row>
       </v-container>
     </v-main>
-
-    <v-footer color="cyan" app absolute>
-      <v-spacer></v-spacer>
-      <span class="white--text">&copy;{{ new Date().getFullYear() }}</span>
-    </v-footer>
-  </div>
+  </v-app>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from "vue-property-decorator";
-@Component({
-  // name: "baseline-flipped",
-  // components: {
-  //   "baseline-flipped": BaselineFlipped,
-  // },
-})
-export default class BaselineFlipped extends Vue {
+import { Vue, Component, Prop } from "vue-property-decorator";
+@Component({})
+export default class GoogleKeep extends Vue {
+  @Prop(String)
+  source!: string;
+
   drawer = null;
+
+  items = [
+    { icon: "lightbulb_outline", text: "Notes" },
+    { icon: "touch_app", text: "Reminders" },
+    { divider: true },
+    { heading: "Labels" },
+    { icon: "add", text: "Create new label" },
+    { divider: true },
+    { icon: "archive", text: "Archive" },
+    { icon: "delete", text: "Trash" },
+    { divider: true },
+    { icon: "settings", text: "Settings" },
+    { icon: "chat_bubble", text: "Trash" },
+    { icon: "help", text: "Help" },
+    { icon: "phonelink", text: "App downloads" },
+    { icon: "keyboard", text: "Keyboard shortcuts" },
+  ];
 }
 </script>
 
 <style>
+#keep .v-navigation-drawer__border {
+  display: none;
+}
 </style>
